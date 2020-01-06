d830b1904a4257bfa8aa49f8a97c2ae091734ff8
Brandon Weaver <a7xncob@gmail.com>
2020-01-06 03:48:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20200106034317) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.float "balance"
+    t.string "balance"
   end
   
 end
