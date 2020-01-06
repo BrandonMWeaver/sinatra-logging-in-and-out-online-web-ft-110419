27a064fb28b1dfa6bb0084c009bf66c44a522287
Brandon Weaver <a7xncob@gmail.com>
2020-01-06 03:48:05 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20200106034317) do
-
+  
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.string "balance"
+    t.float "balance"
   end
-
+  
 end
